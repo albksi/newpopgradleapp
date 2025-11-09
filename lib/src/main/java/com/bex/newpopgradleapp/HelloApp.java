c8234b3a7011749929d028a73ba1be139c741a38
albksi <albksi@yahoo.com>
2025-11-09 02:33:27 +0000

    first project commit print success message

---
+++ lib/src/main/java/com/bex/newpopgradleapp/HelloApp.java
@@ -0,0 +1,9 @@
+package com.bex.newpopgradleapp;
+
+public class HelloApp {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, Gradle!");
+	}
+
+}
---
+++ lib/src/test/java/com/bex/newpopgradleapp/HelloAppTest.java
@@ -0,0 +1,14 @@
+package com.bex.newpopgradleapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloAppTest {
+	
+	@Test
+	public void testHelloWorld() {
+		assertEquals("Hello, Gradle!", "Hello, Gradle!");
+	}
+	
+
+}
